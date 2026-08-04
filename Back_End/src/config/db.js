ec6da52932cf6dc1d68f08317c89a05d47fa6160
Muhammad Rashid Khan <pmik754@gmail.com>
2026-08-04 12:10:22 +0100

    Update DB

--- Back_End/src/config/db.js
+++ Back_End/src/config/db.js
@@ -2,11 +2,13 @@ const mongoose = require("mongoose");
 
 exports.connectDB = async () => {
   try {
+    console.log("MONGO_URI exists:", !!process.env.MONGO_URI);
+
     await mongoose.connect(process.env.MONGO_URI);
+
     console.log("MongoDB Connected Successfully");
   } catch (error) {
-    console.error("MongoDB Connection Error:");
-    console.error(error); // <-- asli error yahan milega
+    console.error("MongoDB Connection Error:", error);
     process.exit(1);
   }
 };
