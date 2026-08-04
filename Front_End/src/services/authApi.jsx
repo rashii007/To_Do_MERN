d31ac4ba45db308ae0049c0c8a1acf4bb74ac55f
Muhammad Rashid Khan <pmik754@gmail.com>
2026-08-04 11:20:48 +0100

    Redeploy with VITE_API_URL

--- Front_End/src/services/authApi.jsx
+++ Front_End/src/services/authApi.jsx
@@ -4,6 +4,7 @@ import axios from "axios";
 // ✅ CORRECT - Vite uses import.meta.env
 const API_BASE_URL =
   import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+console.log("API_BASE_URL:", API_BASE_URL);
 
 const api = axios.create({
   baseURL: API_BASE_URL,
